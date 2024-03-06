3bc653ad198272caae460835dd2edc5222ea0ba2
Oguzhan Yıldız <oguzhan.yildiz@32bit.com.tr>
2024-03-06 17:03:28 +0300

    init project

---
+++ src/main/java/com/sau/dims/DimsApplication.java
@@ -0,0 +1,13 @@
+package com.sau.dims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DimsApplication.class, args);
+	}
+
+}
