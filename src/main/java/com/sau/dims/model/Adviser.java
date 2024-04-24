341c5957a48abce382b733c1a43e38a05ba0d0ad
Yunus Emre <yunustc5461@gmail.com>
2024-04-24 11:46:34 +0300

    adviser space character validation fixed

--- src/main/java/com/sau/dims/model/Adviser.java
+++ src/main/java/com/sau/dims/model/Adviser.java
@@ -20,10 +20,10 @@ public class Adviser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @NotBlank(message = "Adviser's name can not be null!")
-    @Pattern(regexp = "^[a-zA-Z]*$", message = "Name can not contain any non-letter!")
+    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Name can not contain any non-letter!")
     private String name;
     @NotBlank(message = "Department can not be null!")
-    @Pattern(regexp = "^[a-zA-Z]*$", message = "Department can not contain any non-letter!")
+    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Department can not contain any non-letter!")
     private String department;
 
     @OneToMany(mappedBy = "id",cascade = CascadeType.ALL)
