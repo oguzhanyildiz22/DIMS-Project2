caedad5873b632f12de1d1a346b3c727fad74b9c
Oguzhan Yıldız <oguzhan.yildiz@32bit.com.tr>
2024-04-29 18:12:05 +0300

    added adviser-study delete button.

--- src/main/resources/static/js/adviser-study/main.js
+++ src/main/resources/static/js/adviser-study/main.js
@@ -2,6 +2,17 @@
  *
  */
 
+$(document).on("click", ".delete-button", function () {
+    let adviserStudyId = $(this).attr('id');
+    $("#deleteIdEdit").val(adviserStudyId);
+
+    let adviserName = $(this).closest('tr').find('td:nth-child(1)').text();
+    let titleName = $(this).closest('tr').find('td:nth-child(4)').text();
+    $("#deleteNameEdit").val(adviserName);
+    $("#deleteTitleEdit").val(titleName);
+
+});
+
 $(document).ready(function() {
     $('#modals-container').load('modals/adviser-study/modals-adviser-study.html');
 });
